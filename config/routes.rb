929b568b7b52f2d65df2e268b5e861d14ac16bfc
deonjoshua <deonjoshua1997@gmail.com>
2023-11-14 21:17:26 +1100

    Yelp MVP

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root to: "restaurants#index"
+  resources :restaurants, only: [:index, :show, :new, :create] do
+    resources :reviews, only: [:create]
+  end
   # Defines the root path route ("/")
   # root "articles#index"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+puts "Cleaning database..."
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = {name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "chinese"}
+pizza_east =  {name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "italian"}
+nihonbashi = {name: "Nihonbashi", address: "Galle Face Green, Colombo SL", category: "japanese"}
+omnia = {name: "Omnia", address: "South Yarra, Melbourne VIC AUS", category: "french"}
+frites = {name: "Frites", address: "Evergreen Mews, Armadale, Melbourne VIC AUS", category: "belgian"}
+
+[dishoom, pizza_east, nihonbashi, omnia, frites].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+end
+puts "Finished!"
